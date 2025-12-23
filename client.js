0084ece0dd0b427cc4aefbd8a231c54875d72ff5
vickypeng <cxz0917001997.gmail.com>
2025-12-23 22:28:26 +0800

    fix: 更新 API_URL 以指向正確的伺服器地址

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-const API_URL = "https://special-education-platform.zeabur.app";
+const API_URL = "https://special-education-platform-main-peng.zeabur.app/";
 const socket = io(API_URL);
 let currentUser = null;
 
